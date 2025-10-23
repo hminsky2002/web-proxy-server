3a7185d5cf7ee5927f878e49be91924db425d418
Harry Minsky <harry.minsky@gmail.com>
2025-10-23 03:19:29 -0400

    mostly finish server

--- http_parser.py
+++ http_parser.py
@@ -1,62 +1,79 @@
-from collections import defaultdict
+from dataclasses import dataclass
+from typing import Optional
 
-crlf = '\r\n'
+CRLF = '\r\n'
 
-def parse_host_header(header: str) -> dict | None:
-    
+@dataclass
+class HostInfo:
+    host_name: str
+    port: Optional[str] = None
+
+@dataclass
+class HttpRequest:
+    request_line: str
+    headers: dict[str, str]
+    body: str
+    host: HostInfo
+
+def parse_host_header(header: str) -> Optional[HostInfo]:
     if header == " " or not header:
-        return None 
-    
-    host_dict = {}
+        return None
     
     port_idx = header.find(':')
     if port_idx > 0:
-        host_dict['port'] = header[port_idx:]
-        host_dict['host_name'] = header[0:port_idx]
+        return HostInfo(
+            host_name=header[0:port_idx].strip(),
+            port=header[port_idx+1:].strip()
+        )
     else:
-        host_dict['port'] = None
-        host_dict['host_name'] = header
-    
-    return host_dict
-       
+        return HostInfo(host_name=header.strip())
 
-def parse_http_request(data: bytes) -> dict:
-    
-    output_dict = {}
-    
-    parsed_data = data.decode("utf-8").split(crlf)
-    
+def parse_http_request(data: bytes) -> HttpRequest:
+    parsed_data = data.decode("utf-8").split(CRLF)
     request_line = parsed_data[0]
+    headers_raw = parsed_data[1:-2]
     
-    output_dict['request_line'] = request_line
+    header_mappings: dict[str, str] = {}
+    prev_header: Optional[str] = None
     
-    headers = parsed_data[1:-2]
-    
-    header_mappings = defaultdict(str)
-    
-    prev_header = None
-    
-    for s in headers:
-        if s[0] == " ":
-            header_mappings[prev_header] += s   
-            continue
+    for line in headers_raw:
+        if line and line[0] == " ":
+            if prev_header:
+                header_mappings[prev_header] += line
         else:
-            a = s.split(":") 
-            header_mappings[a[0].strip()] = a[1].strip()
-            prev_header = a[0]
+            colon_idx = line.find(":")
+            if colon_idx > 0:
+                header = line[0:colon_idx].strip()
+                value = line[colon_idx+1:].strip()
+                header_mappings[header] = value
+                prev_header = header
     
-    output_dict['headers'] = header_mappings
+    host_info = None
+    if 'Host' in header_mappings:
+        host_info = parse_host_header(header_mappings['Host'])
+    else:
+        raise ValueError("No Host Specified")
     
-    if output_dict['headers']['Host']:
-        output_dict['host'] = parse_host_header(output_dict['headers']['Host'])
+    if host_info == None:
+        raise ValueError("No Host Specified")
     
     body = parsed_data[-1]
     
-    output_dict['body'] = body
-    
-    return output_dict
+    return HttpRequest(
+        request_line=request_line,
+        headers=header_mappings,
+        body=body,
+        host=host_info
+    )
 
-
-        
-        
+def generate_proxy_http_request(request: HttpRequest) -> bytes:
+    headers = request.headers.copy()
     
+    if request.host:
+        port_suffix = f':{request.host.port}' if request.host.port else ''
+        headers['X-Forwarded-For'] = f'{request.host.host_name}{port_suffix}'
+    
+    headers_str = ''.join([f'{key}: {value}{CRLF}' for key, value in headers.items()])
+    proxy_request = f'{request.request_line}{CRLF}{headers_str}{CRLF}{request.body}'
+    
+    return proxy_request.encode('utf-8')
--- server.py
+++ server.py
@@ -1,31 +1,149 @@
 import selectors
 import socket
 from http_parser import *
+from collections import defaultdict
+from dataclasses import dataclass
+import re
+from time import time
+from typing import Optional
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='server.log', encoding='utf-8', level=logging.DEBUG)
 
 sel = selectors.DefaultSelector()
 
-def accept(sock, mask):
-    conn, addr = sock.accept() 
-    conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, read)
+@dataclass
+class CachedRequest:
+    max_age: int
+    response: bytes
+    cached_at: float
 
-def read(conn, mask):
-    data = conn.recv(1000) 
+server_cache = dict[str, CachedRequest]()
+
+def parse_cache_control(cache_control_value: str) -> Optional[int]:
+    if not cache_control_value:
+        return None
+    
+    match = re.search(r'max-age=(\d+)', cache_control_value, re.IGNORECASE)
+    if match:
+        return int(match.group(1))
+    return None
+
+def extract_cache_control_from_response(response: bytes) -> Optional[int]:
+    try:
+        response_str = response.decode('utf-8', errors='ignore')
+        lines = response_str.split('\r\n')
+        
+        for line in lines:
+            if line.lower().startswith('cache-control:'):
+                
+                value = line.split(':', 1)[1].strip()
+                
+                return parse_cache_control(value)
+            
+    except Exception:
+        pass
+    return None
+
+def accept(sock: socket.socket, mask, args):
+    
+    conn, addr = sock.accept()
+    
+    conn.setblocking(False)
+    
+    sel.register(conn, selectors.EVENT_READ, (receive, None))
+
+def receive(conn: socket.socket, mask, addr):
+    data = conn.recv(1000)
     if data:
-        print(parse_http_request(data))
-        conn.send(data)  
+        http_dict = parse_http_request(data)
+        port = http_dict.host.port if http_dict.host.port != None else 80
+        host = http_dict.host.host_name
+        
+        cache_key = http_dict.request_line
+        
+        if cache_key in server_cache:
+            logger.debug('checking cache key', cache_key)
+            cached = server_cache[cache_key]
+            age = time() - cached.cached_at
+            if age < cached.max_age:
+                conn.sendall(cached.response)
+                sel.unregister(conn)
+                conn.close()
+                return
+            else:
+                del server_cache[cache_key]
+        
+        proxy_sock = socket.socket()
+        proxy_sock.setblocking(False)
+        
+        if isinstance(port, str):
+            port = int(port)
+            
+        proxy_sock.connect_ex((host, port))
+        sel.register(proxy_sock, selectors.EVENT_WRITE,
+                     (send_proxy_connection_req, (http_dict, conn, cache_key)))
     else:
         sel.unregister(conn)
         conn.close()
+
+def send_proxy_connection_req(proxy_sock: socket.socket, mask, args):
+    http_dict, client_conn, cache_key = args
+    
+    err = proxy_sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+    
+    if err == 0:
+        
+        sel.modify(proxy_sock, selectors.EVENT_WRITE,
+                   (send_proxy_data, (http_dict, client_conn, cache_key)))
+        
+    else:
+        
+        sel.unregister(proxy_sock)
+        proxy_sock.close()
+        sel.unregister(client_conn)
+        client_conn.close()
+
+def send_proxy_data(proxy_sock: socket.socket, mask, args):
+    http_dict, client_conn, cache_key = args
+    
+    message = generate_proxy_http_request(http_dict)
+    
+    logger.debug('proxy message being sent:', message.decode())
+    
+    proxy_sock.sendall(message)
+    
+    sel.modify(proxy_sock, selectors.EVENT_READ,
+               (receive_proxy_response, (client_conn, cache_key)))
+
+def receive_proxy_response(proxy_sock: socket.socket, mask, args):
+    client_conn, cache_key = args
+    
+    response = proxy_sock.recv(4096)
+    
+    if response:
+        max_age = extract_cache_control_from_response(response)
+        if max_age is not None:
+            cached = CachedRequest(max_age, response, time())
+            server_cache[cache_key] = cached
+        
+        client_conn.sendall(response)
+    
+    sel.unregister(proxy_sock)
+    proxy_sock.close()
+    sel.unregister(client_conn)
+    client_conn.close()
 
 sock = socket.socket()
 sock.bind(('localhost', 1234))
 sock.listen(100)
 sock.setblocking(False)
-sel.register(sock, selectors.EVENT_READ, accept)
+sel.register(sock, selectors.EVENT_READ, (accept, None))
 
 while True:
     events = sel.select()
     for key, mask in events:
-        callback = key.data
-        callback(key.fileobj, mask)
+        callback = key.data[0]
+        args = key.data[1]
+        callback(key.fileobj, mask, args)
